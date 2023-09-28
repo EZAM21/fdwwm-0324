de925ce859fb3a63d02430a7163046c0107849c4
EZAM21 <johanh497@gmail.com>
2023-09-28 16:20:53 +0400

    mon troisième dépot

---
+++ db.js
@@ -0,0 +1,78 @@
+console.log("Coucou les amis");
+
+//Import du paquet mysql pour pouvoir l'utiliser ici
+import mysql from "mysql"
+
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'restful_api'
+});
+ 
+connection.connect((err) => {
+        if(err) throw err
+        console.log("ok");
+});
+
+
+
+function getAllUsers() {
+  connection.query('SELECT * FROM users', (error, results) => {
+      if (error) throw error;
+
+      for (let i = 0; i < results.length; i++) {
+          console.log('#ID : ', results[i].id);
+          console.log('Nom : ', results[i].nom);
+          console.log('Age : ', results[i].age);
+      }
+
+  });
+
+  connection.end();
+}
+
+// getAllUsers();
+
+function getUserId() {
+  connection.query('SELECT iduser FROM users', (error, results) => {
+    if(error) throw error;
+
+    for ( let i = 0; i < results.length; i++){
+      console.log("#ID : ", results[i].iduser);
+    }
+  })
+};
+
+// getUserId();
+
+function getTaskFromNameUser() {
+  connection.query('SELECT Nom FROM users', (error, results) => {
+    if(error) throw error;
+
+    for ( let i = 0; i < results.length; i++){
+      console.log("Nom : ", results[i].Nom);
+    }
+  })
+};
+
+getTaskFromNameUser();
+
+
+// //test connexion
+// db.connect((err) => {
+//   //Si j'ai une erreur je l'affiche dans le terminal
+//   if ((err) => {
+//     throw err;
+//   }
+//   console.log("connected to dabatase")
+  
+
+// )});
+
+// //get task data 
+// const getAllTask =  => {
+
+// }
+
+// const getTaskFromNameUser = (id) 
