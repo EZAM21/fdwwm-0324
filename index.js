e9834ee02c9add186b5b480640dcf072b91548de
EZAM21 <johanh497@gmail.com>
2023-10-05 07:34:19 +0400

    new commit

--- index.js
+++ index.js
@@ -1,2 +1,50 @@
 import {getAllUsers, getUserId, getTaskFromNameUser } from './controllers/user.js'
 getAllUsers();
+
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//     res.end('Voilà la réponse du serveur !');
+// });
+
+// server.listen(process.env.PORT || 3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Le serveur est en cours d'exécution sur le port ${port}`);
+// });
+// const express = require('express');
+// const app = express();
+// const port = 3000; // Vous pouvez spécifier le port de votre choix
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Le serveur est en cours d'exécution sur le port ${port}`);
+// });
---
+++ routers/task.js
@@ -0,0 +1,33 @@
+//import express
+import express from "express";
+
+//create a router
+export const taskRouter = express.Router();
+
+//import controller
+import { getAlltask, getTaskById, postTaskById, deleteTaskById, editTaskById } from '../controllers/user.js'
+
+//route get sur l'url /tasks pour obtenir toutes les taches
+taskRouter.get('/tasks', (req, res) => {
+    getAlltask(req, res);
+})
+
+//route du controller « getTaskById » qui prend en parametre l’id d’une tâche.
+taskRouter.get('/tasks/:id', (req, res) => {
+    getTaskById(req, res);
+})
+
+//route du controller postTaskById 
+taskRouter.post('/tasks/:id', (req, res) => {
+    postTaskById(req, res);
+})
+
+//route du controller deleteTaskById
+taskRouter.delete('/tasks/:id', (req, res) => {
+    deleteTaskById(req, res);
+})
+
+//route du controller editTaskById
+taskRouter.put('/tasks/:id', (req, res) => {
+    editTaskById(req, res);
+})
