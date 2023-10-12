ab1357c92cb2f744f8fb9789ba959188d73681a3
EZAM21 <johanh497@gmail.com>
2023-10-12 14:02:08 +0400

    Ajout de l'ORM

--- routeur/taskRouteur.js
+++ routeur/taskRouteur.js
@@ -10,3 +10,23 @@ import { getTaskById } from "../controlleur/taskController.js";
 taskRouteur.get('/task/:id',(req,res) => {
         getTaskById(req,res)
 })
+
+//route du controller « getTaskById » qui prend en parametre l’id d’une tâche.
+taskRouteur.get('/tasks/:id', (req, res) => {
+        getTaskById(req, res);
+})
+    
+//route du controller postTaskById 
+taskRouteur.post('/tasks/:id', (req, res) => {
+        postTaskById(req, res);
+})
+    
+//route du controller deleteTaskById
+taskRouteur.delete('/tasks/:id', (req, res) => {
+        getTaskById(req, res);
+})
+    
+//route du controller editTaskById
+taskRouteur.put('/tasks/:id', (req, res) => {
+        getTaskById(req, res);
+})
