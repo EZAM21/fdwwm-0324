30d22f01164d55a63bbb79e97439453ac6c32adb
EZAM21 <johanh497@gmail.com>
2023-10-16 15:57:28 +0400

    New commit

--- routeur/userRouteur.js
+++ routeur/userRouteur.js
@@ -10,23 +10,3 @@ import { getAllUsers } from "../controlleur/userController.js";
 userRouteur.get('/users',(req,res) => {
         getAllUsers(req,res)
 })
-
-//route du controller « getTaskById » qui prend en parametre l’id d’une tâche.
-userRouteur.get('/tasks/:id', (req, res) => {
-        getTaskById(req, res);
-})
-    
-//route du controller postTaskById 
-userRouteur.post('/tasks/:id', (req, res) => {
-        postTaskById(req, res);
-})
-    
-//route du controller deleteTaskById
-userRouteur.delete('/tasks/:id', (req, res) => {
-        deleteTaskById(req, res);
-})
-    
-//route du controller editTaskById
-userRouteur.put('/tasks/:id', (req, res) => {
-        editTaskById(req, res);
-})
